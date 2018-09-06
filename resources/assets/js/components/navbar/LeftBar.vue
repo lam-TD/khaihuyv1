a3fcd3d279beb5ac8ff9e93c00481ef5cc1dbde3
lamtranduc <32539012+lam-TD@users.noreply.github.com>
2018-09-06 17:17:15 +0700

    up

--- resources/assets/js/components/navbar/LeftBar.vue
+++ resources/assets/js/components/navbar/LeftBar.vue
@@ -74,7 +74,7 @@
                     custom.setAttribute('src',"public/template/js/custom.js");
                     custom.setAttribute('id','custom');
                     document.body.appendChild(custom);
-                },2000);
+                },4000);
             },
             beforeDestroy() {
 
