552163663d9500690faa01737ac639c1985d3366
lamtranduc <32539012+lam-TD@users.noreply.github.com>
2018-09-06 16:50:00 +0700

    up

--- resources/assets/js/helper/general.js
+++ resources/assets/js/helper/general.js
@@ -23,10 +23,10 @@ export function initialize(store, router) {
     })
 
 
-    // axios.interceptors.response.use(null, (error) => {
-    //     if(error.response.status == 401){
-    //         store.commit('logout');
-    //         router.push('/');
-    //     }
-    // });
+    axios.interceptors.response.use(null, (error) => {
+        if(error.response.status == 401){
+            store.commit('logout');
+            router.push('/');
+        }
+    });
 }
